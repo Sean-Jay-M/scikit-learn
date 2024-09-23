9c318b947cef1d0b3dc6a97ede273957a0f0da61
Sean Moiselle <91853278+Sean-Jay-M@users.noreply.github.com>
2024-09-23 12:28:30 +0100

    DOC add link to plot_svm_nonlinear in SVC

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -640,6 +640,10 @@ class SVC(BaseSVC):
     To learn how to tune SVC's hyperparameters, see the following example:
     :ref:`sphx_glr_auto_examples_model_selection_plot_nested_cross_validation_iris.py`
 
+    To learn how to tune the regularization parameter ``C```and its affect on
+    the decision boundary see:
+    :ref:`sphx_glr_auto_examples_svm_plot_svm_nonlinear`
+
     Read more in the :ref:`User Guide <svm_classification>`.
 
     Parameters
